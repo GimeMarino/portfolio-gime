714773e46973b1559780101eacc2c06c955d74cc
Gime Marino <gimemarino@gmail.com>
2025-05-29 13:26:07 -0300

    Versión lista para producción

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="text-green-400 font-mono p-10">
+      <h1>Hola, soy Gime 👩‍🎨</h1>
+      <p>Este es mi portfolio online.</p>
+    </div>
+  )
+}
